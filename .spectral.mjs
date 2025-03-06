f24ea25ed5d04690a8299cc1601c3e8a808d5d57
maitheeli.deshpande@smartbear.com <maitheeli.deshpande@smartbear.com>
2025-03-06 11:32:08 +0530

    Modified 6 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-19982.stoplight-dev.com/api/v1/projects/cHJqOjU4MQ/spectral.js?branch_slug=main&token=91900ca1-9f44-463c-9e40-047434514691";
+export default { extends: ruleset };
---
+++ docs/test-article.md
@@ -0,0 +1,7 @@
+---
+stoplight-id: t3r1t3w11we7x
+---
+
+# test-article
+
+The beginning of an awesome article...
